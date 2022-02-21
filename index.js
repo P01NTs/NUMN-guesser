91b09dcf5615093d887e92549dba8a4d8d461c8b
P01NTs <mohhdiablotin@gmail.com>
2022-02-21 18:00:17 +0100

    1.1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+/////////////////////////////////////////////////////////////
 const champ = document.getElementById("input");
 const submit = document.getElementById("cc");
 var random = (Math.random() * 10).toFixed();
+
 champ.onkeyup = () => {
+  //is there a value in the input?
   let value = champ.value;
   if (value.trim() != 0) {
     submit.classList.add("active");
@@ -9,48 +12,68 @@ champ.onkeyup = () => {
     submit.classList.remove("active");
   }
 };
-
-
-submit.addEventListener("click", () => {
+///////////////////////////////////////////function that happen when validatin the value
+function validate() {
   let value = parseInt(champ.value); //getting user entered value
   let body = document.querySelector("body");
-  if (value == random) {
+
+  function endGame(win) {
+    //winning or losing
     let creatFieldset = document.createElement("fieldset");
     let creatH1 = document.createElement("h1");
     let creatButton = document.createElement("a");
-    creatFieldset.className = "winner";
-    creatButton.className = "new";
-    creatH1.innerHTML = "WINNER WINNER CHICKEN DINNER";
-    creatButton.innerHTML = "New try";
-    //n9ololo gg
+    creatFieldset.className = "winner"; //creat className
+    creatButton.className = "new"; //creat className
+
+    if (win) {
+      creatH1.innerHTML = "WINNER WINNER CHICKEN DINNER";
+      creatButton.innerHTML = "New try";
+    } else {
+      creatH1.innerHTML = "LOSER LOSER DOUBLE LOSER";
+      creatButton.innerHTML = "Try again";
+    }
     body.appendChild(creatFieldset);
     creatFieldset.appendChild(creatH1);
     creatFieldset.appendChild(creatButton);
     creatButton.addEventListener("click", () => {
       window.location.reload();
     });
+    return;
+  }
+
+  function mistake(chances) {
+    //indicateur en cas d'érreur
+    let indicator = document.querySelector("fieldset > p");
+    document.querySelector("span").innerHTML = chances;
+    value > random
+      ? (indicator.innerHTML = "LOWER !!")
+      : (indicator.innerHTML = "HIGHER !!");
+  }
+
+  if (value == random) {
+    //checking value of the user
+    endGame(true);
+    champ.blur();
   } else {
     //na9so les chances
     let chance = parseInt(document.querySelector("span").innerHTML);
     if (chance > 1) {
       chance -= 1;
-      
-      document.querySelector("span").innerHTML = chance;
+      mistake(chance);
     } else {
-      let creatFieldset = document.createElement("fieldset");
-      let creatH1 = document.createElement("h1");
-      let creatButton = document.createElement("a");
-      creatFieldset.className = "winner";
-      creatButton.className = "new";
-      creatH1.innerHTML = "LOSER LOSER DOUBLE LOSER";
-      creatButton.innerHTML = "Try again";
-
-      body.appendChild(creatFieldset);
-      creatFieldset.appendChild(creatH1);
-      creatFieldset.appendChild(creatButton);
-      creatButton.addEventListener("click", () => {
-        window.location.reload();
-      });
+      endGame(false);
+      champ.blur();
     }
   }
+}
+///////////////////////////////////////////end
+
+//validating Listenrs
+champ.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    validate();
+  }
+});
+submit.addEventListener("click", () => {
+  validate();
 });
